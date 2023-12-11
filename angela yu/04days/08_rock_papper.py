4be4536ac265e9978b5fa96ffd8a45d7865017f5
unknow <t0rjan@kali.unknow>
2023-12-11 10:29:27 +0530

    day 4 completed

--- angela yu/04days/08_rock_papper.py
+++ angela yu/04days/08_rock_papper.py
@@ -1,6 +1,58 @@
-game = int(input("What do you choose ? \nType 0 for Rock,1 for Papper or 2 for Scissors :"))
-rock ="close_palm"
-papper="open_palm"
-scissors = "two finger"
-possiblities = ["rock","papper","scissors"]
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+user= int(input("What do you choose ? \nType 0 for Rock,1 for Papper or 2 for Scissors :\n"))
+choose = [rock,paper,scissors]
+if user >= 3 or user <0 :
+    print("please enter a valid input ")
+else :
+    print("you choosed")
+    print(f"{choose[user]}")
+
+computer = random.randint(0,2)
+print("computer chosen")
+print(f"{choose[computer]}")
+
+
+if  computer == 2 and user == 0:
+    print("you won!")
+elif computer == 0 and user == 2:
+    print("you lose !")
+elif computer > user :
+    print("you lose!")
+elif user > computer:
+    print("you win!")
+elif computer == user:    
+    print("It's a tie!")
+else :
+    print("Opps! just restart the game .")
+
+
+
+
 
--- angela yu/04days/tempCodeRunnerFile.py
+++ angela yu/04days/tempCodeRunnerFile.py
@@ -1,3 +1,4 @@
-rock ="close_palm"
-papper="open_palm"
-scissors = "two finger"
+# rock ="close_palm"
+# papper="open_palm"
+# scissors = "two finger"
+user = input("choose any one ROCK , PAPER or SCISSORS : \n").lower
